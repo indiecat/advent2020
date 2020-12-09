abcbed1bdc477de549f6cf8c99636bd1bb8eb156
indiecat <indiecat@users.noreply.github.com>
2020-12-10 01:43:58 +1000

    Complete day 8

--- src/advent2020/day8.clj
+++ src/advent2020/day8.clj
@@ -1,8 +1,6 @@
 (ns advent2020.day8
   (:require
     [clojure.string :as str]
-    [clojure.set :as set]
-    [clojure.walk :as walk]
     [advent2020.core :refer [parse-int]]))
 
 (set! *warn-on-reflection* true)
@@ -36,9 +34,54 @@
 #_(time (part1 instructions))
 
 
-(defn part2 []
-)
+(defn attempt-to-run [instructions]
+  (let [accumulator (atom 0)
+        termination-ix (count instructions)]
+    (loop [current-ix 0
+           used-ixs #{}]
+      (cond
+        (contains? used-ixs current-ix) [:looped used-ixs]
+        (= current-ix termination-ix) [:terminated @accumulator]
+        :else (recur
+               (+ current-ix
+                  (run-instruction (nth instructions current-ix) accumulator))
+               (conj used-ixs current-ix))))))
 
-#_(time (part2))
+(def indexes-to-test (second (attempt-to-run instructions)))
+
+(def indexes-to-alter (filter
+                       (fn [ix] (-> (nth instructions ix)
+                                    (:op)
+                                    (not= "acc")))
+                       indexes-to-test))
+
+(defn run-instruction-inverted [instruction accumulator]
+  (case (instruction :op)
+    "jmp" 1
+    "acc" (do (swap! accumulator #(+ (instruction :arg) %)) 1)
+    "nop" (instruction :arg)))
+
+(defn attempt-to-run-with-inverted [instructions inverted-ix]
+  (let [accumulator (atom 0)
+        termination-ix (count instructions)]
+    (loop [current-ix 0
+           used-ixs #{}]
+      (cond
+        (contains? used-ixs current-ix) [:looped used-ixs]
+        (= current-ix termination-ix) [:terminated @accumulator]
+        :else (recur
+               (+ current-ix
+                  (if (= current-ix inverted-ix)
+                    (run-instruction-inverted (nth instructions current-ix) accumulator)
+                    (run-instruction (nth instructions current-ix) accumulator)))
+               (conj used-ixs current-ix))))))
+
+(defn part2 [instructions]
+  (->> (map #(attempt-to-run-with-inverted instructions %) indexes-to-alter)
+    (filter #(= (first %) :terminated))
+    (first)
+    (second)))
+
+#_(time (part2 instructions))
 
 #_(require 'advent2020.day8 :reload)
